f4791999e839dfe66d44aedc8980106cc73a85d2
Matt Burke <spraints@gmail.com>
2011-10-24 14:45:04 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SandwichAnatomy::Application.config.secret_token = '4a1d9bcc78a2ab512b7849f9b277be0a701b2d020515dc73ef880f5e69665b23089fe8f613d06d5898197789116c2aba74d227abe735db94ff06d85f85af211f'
